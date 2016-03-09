4962560e24f7ff714d6e6649390c760179ef3d04
Andrew Scott <amscott@openmailbox.org>
2016-03-09 18:27:20 +0000

    installed hound

--- mix.exs
+++ mix.exs
@@ -13,13 +13,20 @@ defmodule Elixcommerce.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
-    [mod: {Elixcommerce, []},
-     applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+  [
+    mod: {PhoenixCommerce, []},
+    applications: applications(Mix.env)
+  ]
+  end
+
+  defp applications(:test) do
+    [:hound | applications(:dev)]
+  end
+
+  defp applications(_) do
+     [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
+      :phoenix_ecto, :postgrex]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,13 +37,16 @@ defmodule Elixcommerce.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.1"},
+    [
+     {:phoenix, "~> 1.1.1"},
      {:phoenix_ecto, "~> 2.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.3"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:hound, "~> 0.7.6", only: :test}
+   ]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
